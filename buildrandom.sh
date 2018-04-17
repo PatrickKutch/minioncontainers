bbdae7c45c9c897c97daf910faf62827eac33cad
Patrick Kutch <Patrick.Kutch@gmail.com>
2018-04-17 01:27:44 -0700

    updated builds to do a clone outside of container build, to reduce container size

--- buildrandom.sh
+++ buildrandom.sh
@@ -1,2 +1,12 @@
+cd random
+rm -f -r  Board-Instrumentation-Framework
+git clone https://github.com/intel/Board-Instrumentation-Framework.git
+# don't need Marvin or Oscar
+rm -r -f Board-Instrumentation-Framework/Oscar
+rm -r -f Board-Instrumentation-Framework/Marvin
+rm -r -f Board-Instrumentation-Framework/*.pdf
+
+cd ..
 docker build -t patrickkutch/biff-minion-random random
 docker push patrickkutch/biff-minion-random
+
--- buildsysinfo.sh
+++ buildsysinfo.sh
@@ -1,2 +1,11 @@
-docker build --no-cache -t patrickkutch/biff-minion-sysinfo systeminfo
+cd systeminfo
+rm -f -r  Board-Instrumentation-Framework
+git clone https://github.com/intel/Board-Instrumentation-Framework.git
+# don't need Marvin or Oscar
+rm -r -f Board-Instrumentation-Framework/Oscar
+rm -r -f Board-Instrumentation-Framework/Marvin
+rm -r -f Board-Instrumentation-Framework/*.pdf
+
+cd ..
+docker build -t patrickkutch/biff-minion-sysinfo systeminfo
 docker push patrickkutch/biff-minion-sysinfo

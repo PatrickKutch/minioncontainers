5784c2463886c662708b8b2bc89ecf4513e2220a
root <root@nd-r2208-53.jf.intel.com>
2018-08-22 14:14:40 +0000

    adding generic build

---
+++ buildgeneric.sh
@@ -0,0 +1,12 @@
+cd generic
+rm -f -r  Board-Instrumentation-Framework
+git clone https://github.com/intel/Board-Instrumentation-Framework.git
+# don't need Marvin or Oscar
+rm -r -f Board-Instrumentation-Framework/Oscar
+rm -r -f Board-Instrumentation-Framework/Marvin
+rm -r -f Board-Instrumentation-Framework/*.pdf
+
+cd ..
+
+docker build -t patrickkutch/biff-minion-generic generic
+docker push patrickkutch/biff-minion-generic

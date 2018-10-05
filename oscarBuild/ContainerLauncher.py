554fa82a7dc05206520bd77022dc86ec371ff311
Patrick Kutch <Patrick.Kutch@gmail.com>
2018-10-05 15:33:38 +0000

    genericed oscar build

--- oscarBuild/ContainerLauncher.py
+++ oscarBuild/ContainerLauncher.py
@@ -4,28 +4,24 @@ import os
 
 def main():
     parser = argparse.ArgumentParser(description='Minion Launcher.')
-    parser.add_argument("-o","--oscar",help='specifies the IP:PORT of target Oscar',required=True,type=str)
-    parser.add_argument("-n","--namespace", help="Namespace for collectors",required=True,type=str)
-    parser.add_argument("-f","--frequency", help="Frequency for collection, default is 2000",type=int)
+    parser.add_argument("-t","--target",help='specifies the IP:PORT of target Oscar or Marvin',required=True,type=str)
+    parser.add_argument("-p","--port",help="the UDP port to listen on default is 3232",type=int,default=3232)
+    parser.add_argument("-i","--id",help="the Oscar ID, default is 'ContainerizedOscar'",type=str,default="ContainerizedOscar")
 
     args = parser.parse_args()
 
-    parts=args.oscar.split(":")
-    oscarIP = parts[0]
-    oscarPort = parts[1]
+    parts=args.target.split(":")
+    marvinIP = parts[0]
+    marvinPort = parts[1]
     int(parts[1]) # port should be an int
-
-    frequency="2000"
-    if None != args.frequency:
-        frequency = args.frequency
-    
+   
     envVars=dict(os.environ)
-    envVars["OscarIP"]=oscarIP
-    envVars["oscarPort"]=oscarPort
-    envVars["Namespace"]=args.namespace
-    envVars["Frequency"]=str(frequency)
-    minionProc = subprocess.Popen(["python", "Minion.py", "-i", "systeminfo.xml"],env=envVars)
-    minionProc.wait()
+    envVars["MarvinIP"]=marvinIP
+    envVars["MarvinPort"]= marvinPort
+    envVars["ListenPort"] = str(args.port)
+    print("Launching")
+    oscarProc = subprocess.Popen(["python", "Oscar.py", "-i", "OscarConfig.xml"],env=envVars)
+    oscarProc.wait()
     
 if __name__ == "__main__":
     try:
